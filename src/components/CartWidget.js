ef1a58b242ef2ed7f0d0c091ac6c0d41146b92b2
Lean <lean_rap50rhotmail.com>
2021-01-28 00:24:50 -0300

    Cart View Fix

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,15 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import { CartContext } from './CartContext'
 
 
 
 export default function CartWidget() {
-  const { quantity } = useContext(CartContext)
+  const { cart } = useContext(CartContext)
+  const [ cartNum, setCartNum ] = useState(0)
+  
+  let cartItemsNums = cart.map(item =>{  
+    return item.amount
+  })
+  
+  useEffect(()=>{
+    setCartNum(cartItemsNums.reduce((a, b)=> a + b,0)) 
+  },[cartItemsNums])
+
+ 
+
+  console.log("cartNum", cartNum )
+
   return (
     <li>
       <Link to="/cart"><a className="large material-icons">shopping_cart</a></Link>
-      { quantity>0 && <a className="red darken-2 btn-floating btn-small halfway-fab waves-effect waves-light red"><i>{quantity}</i></a>}
+      { cartNum>0 && <a className="red darken-2 btn-floating btn-small halfway-fab waves-effect waves-light red"><i>{cartNum}</i></a>}
     </li>
     
   );
